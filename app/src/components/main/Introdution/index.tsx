d79204198c3df6408ae44db83a42238c0aaa6e4a
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-03-01 20:34:25 +0900

    Refactor: Introdution 컴포넌트 style 코드 분리

--- app/src/components/main/Introdution/index.tsx
+++ app/src/components/main/Introdution/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import ProfileImage from '../ProfileImage';
+import { Background, Wrapper, SubTitle, Title } from './style';
 
 const Introdution = () => {
   return (
@@ -15,32 +15,5 @@ const Introdution = () => {
     </Background>
   );
 };
-
-const Background = styled.div`
-  width: 100%;
-  background-image: linear-gradient(60deg, #29323c 0%, #485563 100%);
-  color: #ffffff;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-  width: 768px;
-  height: 400px;
-  margin: 0 auto;
-`;
-
-const SubTitle = styled.div`
-  font-size: 20px;
-  font-weight: 400;
-`;
-
-const Title = styled.div`
-  margin-top: 5px;
-  font-size: 35px;
-  font-weight: 700;
-`;
 
 export default Introdution;
