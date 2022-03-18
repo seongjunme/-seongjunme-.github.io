4862c7cd0f993891cc6a34dfd4711e2144550d71
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-03-18 21:31:54 +0900

    Fix: 블로그의 카테고리 기능 버그 수정
    - All 카테고리가 포스트의 총 개수가 아닌 카테고리의 총 개수로 표시되는 현상 수정

--- app/src/components/blog/BlogList/index.tsx
+++ app/src/components/blog/BlogList/index.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from 'react';
 import PostItem from '../../common/PostItem';
 import { Wrapper } from './style';
 import { PostListType } from 'types/post.types';
+import useInfiniteScroll from 'hooks/useInfiniteScroll';
 
 interface Props {
   posts: PostListType[];
@@ -21,9 +22,11 @@ const BlogList: React.FC<Props> = ({ posts, selectedCategory }) => {
     [selectedCategory],
   );
 
+  const { targetRef, postsByPage } = useInfiniteScroll(filteredPosts);
+
   return (
-    <Wrapper>
-      {filteredPosts.map(
+    <Wrapper ref={targetRef}>
+      {postsByPage.map(
         ({
           node: {
             id,
