01ccec822855eee12e5ba51b529488835674d760
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-03-22 15:15:03 +0900

    Refact: useFullPage 훅 내부의 함수에 useCallback 추가
    - 함수 재 선언 방지

--- app/src/hooks/useFullPage.tsx
+++ app/src/hooks/useFullPage.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject, useState, useRef, useEffect } from 'react';
+import { MutableRefObject, useState, useRef, useEffect, useCallback } from 'react';
 import debounce from 'utils/debounce';
 
 const PAGE_NAMES = ['About', 'Project', 'Blog', 'Contact'];
@@ -8,30 +8,29 @@ const useFullPage = ({ maxPageCount }: { maxPageCount: number }) => {
   const currentPage = useRef(0);
   const [currentPageName, setCurrentPageName] = useState<string>(PAGE_NAMES[currentPage.current]);
 
-  const scrollToCurrentPage = () => {
+  const scrollToCurrentPage = useCallback(() => {
     outerRef.current?.scrollTo({
       top: window.innerHeight * currentPage.current,
       left: 0,
       behavior: 'smooth',
     });
-  };
+  }, []);
 
-  const scrollDown = () => {
+  const scrollDown = useCallback(() => {
     currentPage.current += 1;
     scrollToCurrentPage();
     setCurrentPageName(PAGE_NAMES[currentPage.current]);
-  };
+  }, []);
 
-  const scrollUp = () => {
+  const scrollUp = useCallback(() => {
     currentPage.current -= 1;
     scrollToCurrentPage();
     setCurrentPageName(PAGE_NAMES[currentPage.current]);
-  };
+  }, []);
 
   useEffect(() => {
     const wheelHandler = debounce((e: WheelEvent) => {
       e.preventDefault();
-
       const { deltaY } = e;
 
       if (deltaY > 0 && currentPage.current < maxPageCount) {
@@ -56,14 +55,14 @@ const useFullPage = ({ maxPageCount }: { maxPageCount: number }) => {
     };
   });
 
-  const onClickNavBar = (e: React.MouseEvent<HTMLElement>) => {
+  const onClickNavBar = useCallback((e: React.MouseEvent<HTMLElement>) => {
     const {
       currentTarget: { innerText },
     } = e;
     currentPage.current = PAGE_NAMES.indexOf(innerText);
     scrollToCurrentPage();
     setCurrentPageName(innerText);
-  };
+  }, []);
 
   return { outerRef, currentPageName, onClickNavBar };
 };
