8c7d2c01b00969972d487e46024cdc4ba82af7be
seongjunme <72444675+seongjunme@users.noreply.github.com>
2022-03-23 15:37:16 +0900

    Fix: 모바일 스크린 높이 지정 함수 실행 문제 수정
    - 함수 선언만 하고 실행하지 않았던 문제 수정

--- app/src/hooks/useFullPage.tsx
+++ app/src/hooks/useFullPage.tsx
@@ -53,13 +53,15 @@ const useFullPage = ({ maxPageCount }: { maxPageCount: number }) => {
     return () => {
       window.removeEventListener('resize', scrollToCurrentPage);
     };
-  });
+  }, []);
 
   useEffect(() => {
     const setScreenSize = () => {
       const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty('--vh', `${vh}px`);
     };
+
+    setScreenSize();
 
     window.addEventListener('resize', setScreenSize);
   }, []);
